e8fe0bd150ddc46455742ea5fae5b9a261e927d9
harshablast <harshablast@gmail.com>
2019-11-29 22:46:47 +0530

    Added file structure of project. Navbar with logo implemented. Jumbotron element implemented.

---
+++ src/scripts/typewriter.js
@@ -0,0 +1,10 @@
+// import TypeIt from 'typeit';
+
+new TypeIt('#typewriter', {
+  strings: ["For Everyone.", "Made Simple.", "Demystified"],
+  speed: 100,
+  breakLines: false,
+  waitUntilVisible: true,
+  lifeLike: true,
+  loop: true
+}).go();
